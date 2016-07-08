4e79cd67b8be315c5553923f995ab8391b6d72b4
Philip Goh <philip.wj.goh@gmail.com>
2016-07-08 21:53:04 +0100

    Making problem view parameterised

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -8,4 +8,10 @@ describe("LearnJS", function(){
     learnjs.showView("");
     expect($(".view-container .landing-view").length).toEqual(1);
   });
+
+  it("passes the has view parameter to the show view function", function() {
+    spyOn(learnjs, "problemView");
+    learnjs.showView("#problem-42");
+    expect(learnjs.problemView).toHaveBeenCalledWith("42");
+  });
 });
